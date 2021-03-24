4058d039901c548f136dde76542e3a07df9b6e9e
mostafa <me.cssign@gmail.com>
2021-03-24 03:17:41 +0200

    import flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     AppRoutingModule,
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule    
+    MatToolbarModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
