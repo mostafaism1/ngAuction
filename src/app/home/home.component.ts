a3f69b6a902b395bb10d7cc41e0ce8a90459f8eb
mostafa <me.cssign@gmail.com>
2021-03-24 06:16:06 +0200

    display a responsive grid of products in the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product, ProductService } from '../shared/services/product.service';
+import { MediaObserver } from '@angular/flex-layout';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'nga-home',
@@ -7,7 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  readonly columns$: Observable<number>;
+  readonly products$: Observable<Product[]>;
+
+  readonly breakpointsToColumnsNumber = new Map([
+    ['xs', 1],
+    ['sm', 2],
+    ['md', 3],
+    ['lg', 4],
+    ['xl', 5],
+  ]);
+
+  constructor(private media: MediaObserver, private productService: ProductService) {
+    this.products$ = this.productService.getAll();
+
+    this.columns$ = this.media.media$
+      .pipe(
+        map(mc => <number>this.breakpointsToColumnsNumber.get(mc.mqAlias))
+      );
+  }
 
   ngOnInit(): void {
   }
