c5d5578636a331c3b407dd7a2e64ff4004176ec7
mostafa <me.cssign@gmail.com>
2021-03-24 04:19:20 +0200

    define a product interface

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
 
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  imageUrl: string;
+  description: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
