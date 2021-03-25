d6d52caaea16c760e225d3b4491ae9de094a310d
mostafa <me.cssign@gmail.com>
2021-03-25 13:35:14 +0200

    generate product-detail component

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ProductComponent } from './product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 
 
 @NgModule({
-  declarations: [ProductComponent],
+  declarations: [ProductComponent, ProductDetailComponent],
   imports: [
     CommonModule,
     FlexLayoutModule
