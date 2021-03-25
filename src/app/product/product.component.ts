3acc3a7a1c6d813e893e93369b0db2f9b712024e
mostafa <me.cssign@gmail.com>
2021-03-25 15:28:20 +0200

    implement product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
+import { Product, ProductService } from '../shared/services/product.service';
 
 @Component({
   selector: 'nga-product',
@@ -7,7 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  product$: Observable<Product>;
+  suggestedProducts$: Observable<Product[]>;
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) {
+
+    this.product$ = this.route.paramMap
+      .pipe(
+        map(params => parseInt(params.get('productId') || '', 10)),
+        filter(productId => !!productId),
+        switchMap(productId => this.productService.getById(productId))
+      );
+
+    this.suggestedProducts$ = this.productService.getAll();
+  }
 
   ngOnInit(): void {
   }
