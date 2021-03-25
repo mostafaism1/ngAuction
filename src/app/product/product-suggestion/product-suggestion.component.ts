824a0cdf777b06c5cdd40a033110c077b2e46765
mostafa <me.cssign@gmail.com>
2021-03-25 13:35:57 +0200

    generate product-suggestion component

---
+++ src/app/product/product-suggestion/product-suggestion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nga-product-suggestion',
+  templateUrl: './product-suggestion.component.html',
+  styleUrls: ['./product-suggestion.component.scss']
+})
+export class ProductSuggestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ProductComponent } from './product.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductSuggestionComponent } from './product-suggestion/product-suggestion.component';
 
 
 
 @NgModule({
-  declarations: [ProductComponent, ProductDetailComponent],
+  declarations: [ProductComponent, ProductDetailComponent, ProductSuggestionComponent],
   imports: [
     CommonModule,
     FlexLayoutModule
