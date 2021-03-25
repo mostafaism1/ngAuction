0aa6294ae95513fed1400a0f291c51111230580d
mostafa <me.cssign@gmail.com>
2021-03-25 15:27:50 +0200

    implement product-suggestion component

--- src/app/product/product-suggestion/product-suggestion.component.ts
+++ src/app/product/product-suggestion/product-suggestion.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MediaObserver } from '@angular/flex-layout';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { Product } from 'src/app/shared/services/product.service';
 
 @Component({
   selector: 'nga-product-suggestion',
@@ -7,7 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductSuggestionComponent implements OnInit {
 
-  constructor() { }
+  @Input() products: Product[];
+  readonly columns$: Observable<number>;
+  readonly breakpointsToColumnsNumber = new Map([
+    ['xs', 2],
+    ['sm', 3],
+    ['md', 5],
+    ['lg', 2],
+    ['xl', 3],
+  ]);
+
+  constructor(private media: MediaObserver) {
+
+    this.columns$ = this.media.media$
+      .pipe(
+        map(mc => <number>this.breakpointsToColumnsNumber.get(mc.mqAlias))
+      );
+  }
 
   ngOnInit(): void {
   }
