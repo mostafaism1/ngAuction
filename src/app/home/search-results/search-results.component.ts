1c691c68de3b4f5cde56c013dee26150ecc659e0
mostafa <me.cssign@gmail.com>
2021-03-29 15:32:48 +0200

    implement the search-results component

--- src/app/home/search-results/search-results.component.ts
+++ src/app/home/search-results/search-results.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Product, ProductService } from 'src/app/shared/services/product.service';
 
 @Component({
   selector: 'nga-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.scss']
+  styleUrls: ['./search-results.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SearchResultsComponent implements OnInit {
 
-  constructor() { }
+  products$: Observable<Product[]>
+
+  constructor(private productService: ProductService, private route: ActivatedRoute) {
+    this.products$ = route.queryParams.pipe(
+      switchMap(queryParams => this.productService.search(queryParams)));
+  }
 
   ngOnInit(): void {
   }
