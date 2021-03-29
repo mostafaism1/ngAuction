7de505a338462e4eb4dff287d53c42dc56b8479f
mostafa <me.cssign@gmail.com>
2021-03-29 15:30:41 +0200

    implement the categories component, re-define the home module routes, and remove the home component

--- src/app/home/categories/categories.component.ts
+++ src/app/home/categories/categories.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { Product, ProductService } from 'src/app/shared/services/product.service';
 
 @Component({
   selector: 'nga-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  readonly categoriesNames$: Observable<string[]>;
+  readonly products$: Observable<Product[]>;
+
+  constructor(private productService: ProductService, private route: ActivatedRoute) {
+    this.categoriesNames$ = this.productService.getDistinctCategories().pipe(
+      map(categories => ['all', ...categories]));
+
+    this.products$ = this.route.params.pipe(
+      switchMap(({ category }) => this.getCategory(category)));
+  }
 
   ngOnInit(): void {
+  }
+
+  private getCategory(category: string): Observable<Product[]> {
+    return category.toLowerCase() === 'all'
+      ? this.productService.getAll()
+      : this.productService.getByCategory(category.toLowerCase());
   }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,26 +3,36 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
 
-import { RouterModule } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatGridListModule } from '@angular/material/grid-list';
+import {MatTabsModule} from '@angular/material/tabs';
 import { ProductGridComponent } from './product-grid/product-grid.component';
-import { SearchResultsComponent } from './search-results/search-results.component'
+import { SearchResultsComponent } from './search-results/search-results.component';
 import { CategoriesComponent } from './categories/categories.component'
 
 
+const routes: Route[] = [
+  { path: '', pathMatch: 'full', redirectTo: 'categories' },
+  { path: 'search-results', component: SearchResultsComponent },
+  { path: 'categories',
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'all' },
+      { path: ':category', component: CategoriesComponent },
+    ]
+  }
+];
 
 @NgModule({
   declarations: [HomeComponent, ProductGridComponent, SearchResultsComponent, CategoriesComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild([
-      { path: '', component: HomeComponent }
-    ]),
+    RouterModule.forChild(routes),
     FlexLayoutModule,
-    MatGridListModule
+    MatGridListModule,
+    MatTabsModule
   ]
 })
 export class HomeModule { }
