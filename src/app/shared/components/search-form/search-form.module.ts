f5392db877e201d68d3ff6981293f626c12409e9
mostafa <me.cssign@gmail.com>
2021-03-29 10:46:00 +0200

    import ReactiveFormsModule and material UI modules into search-form module

--- src/app/shared/components/search-form/search-form.module.ts
+++ src/app/shared/components/search-form/search-form.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchFormComponent } from './search-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 
 @NgModule({
   declarations: [SearchFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class SearchFormModule { }
